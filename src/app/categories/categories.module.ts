089701dc6e0fe57c349437d8c9bda98185b03096
andre <andre.costa@tpv-tech.com>
2021-04-20 18:19:04 -0400

    fix unit test

--- src/app/categories/categories.module.ts
+++ src/app/categories/categories.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { HomeComponent } from './components/home/home.component';
 
--- src/app/categories/services/categories.service.spec.ts
+++ src/app/categories/services/categories.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CategoriesService } from './categories.service';
 
 describe('CategoriesService', () => {
   let service: CategoriesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+
+    });
     service = TestBed.inject(CategoriesService);
   });
 
