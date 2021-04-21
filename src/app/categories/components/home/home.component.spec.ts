eea7b21b2870cfbd44480e37b44208f70ca628e1
andre <andre.costa@tpv-tech.com>
2021-04-20 22:58:44 -0400

    fix unit test errors

--- src/app/categories/components/home/home.component.spec.ts
+++ src/app/categories/components/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HomeComponent } from './home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [ HttpClientTestingModule, ReactiveFormsModule ]
     })
     .compileComponents();
   });
